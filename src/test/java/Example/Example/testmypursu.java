3293dff76b0025e33b3c21536fa6a1355066a68b
Anuradhamunde <91406920+Anuradhamunde@users.noreply.github.com>
2023-01-11 16:07:42 +0530

    Update testmypursu.java

--- src/test/java/Example/Example/testmypursu.java
+++ src/test/java/Example/Example/testmypursu.java
@@ -17,12 +17,7 @@ private WebDriver driver;
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-		options.addArguments("start-maximized"); // open Browser in maximized mode
-options.addArguments("disable-infobars"); // disabling infobars
-options.addArguments("--disable-extensions"); // disabling extensions
-options.addArguments("--disable-gpu"); // applicable to windows os only
-options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-options.addArguments("--no-sandbox"); // Bypass OS security model
+		chromeOptions.addArguments("--no-sandbox", "--disable-dev-shm-usage");
 WebDriver driver = new ChromeDriver(options);
         driver =new ChromeDriver(options);
         System.out.println("chromedriver opened successfully");
